7a7e6ba76d97ca210b1e2d519320d0c2c9478e0f
michaelayers3 <michaelgregoryayers@gmail.com>
2023-07-24 13:44:59 -0500

    set up most of the backend, going to take a break and start on the front

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,73 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        //user
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            // .select('-__v -password')
+            // .populate('savedBooks')
+        },
+        //users
+        users: async () => {
+            return User.find()
+            // .select('-__v -password')
+            // .populate('savedBooks')
+        },
+        //me
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id })
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+    Mutation: {
+        //createUser
+        createUser: async (parent, { username, email, password }) => {
+            const user = await User.create({ username, email, password });
+            const token = signToken(user);
+            return { token, user };
+        },
+        //login
+        login: async (parent, { email, password }) => {
+            const user = await Profile.findONe({ email });
+            if (!user) {
+                throw new AuthenticationError('No user found with this email address');
+            }
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw new AuthenticationError('Incorrect credentials');
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        //saveBook
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true, runValidators: true }
+                );
+            };
+            throw new AuthenticationError('You need to be logged in!');
+        },
+        //deleteBook
+        deleteBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                return User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $pull: { savedBooks: { bookId: bookData.bookId } } },
+                    { new: true }
+                );
+            }
+            throw new AuthenticationError('You need to be logged in!');
+        },
+    },
+};
+
+module.exports = resolvers;
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -4,33 +4,23 @@ const jwt = require('jsonwebtoken');
 const secret = 'mysecretsshhhhh';
 const expiration = '2h';
 
-module.exports = {
-  // function for our authenticated routes
-  authMiddleware: function (req, res, next) {
-    // allows token to be sent via  req.query or headers
-    let token = req.query.token || req.headers.authorization;
-
-    // ["Bearer", "<tokenvalue>"]
-    if (req.headers.authorization) {
-      token = token.split(' ').pop().trim();
-    }
-
-    if (!token) {
-      return res.status(400).json({ message: 'You have no token!' });
-    }
-
-    // verify token and get user data out of it
-    try {
-      const { data } = jwt.verify(token, secret, { maxAge: expiration });
-      req.user = data;
-    } catch {
-      console.log('Invalid token');
-      return res.status(400).json({ message: 'invalid token!' });
-    }
-
-    // send to next endpoint
-    next();
-  },
+  module.exports = {
+    authMiddleware: function ({ context }, next) {
+      const { token } = context;
+  
+      if (!token) {
+        throw new Error('You have no token!');
+      }
+  
+      try {
+        const { data } = jwt.verify(token, secret, { maxAge: expiration });
+        context.user = data;
+      } catch {
+        throw new Error('Invalid token');
+      }
+  
+      return next();
+    },
   signToken: function ({ username, email, _id }) {
     const payload = { username, email, _id };
 
